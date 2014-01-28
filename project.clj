0e5c934d4255ae46542034428ce5c6373834d410
James Laver <james@codegems.org>
2014-01-28 18:53:20 +0000

    Version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ring-http-middleware "0.1.0"
+(defproject ring-http-middleware "0.1.1"
   :url "https://github.com/jjl/ring-http-middleware"
   :description "Middleware for ring that helps with HTTP params"
   :license "Eclipse Public License v 1.0")
