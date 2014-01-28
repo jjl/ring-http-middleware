d48bf63d979d6bde95a94b5bfcdc036bc7ca7b59
James Laver <james@codegems.org>
2014-01-28 18:47:28 +0000

    add :license

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject ring-http-middleware "0.1.0"
   :url "https://github.com/jjl/ring-http-middleware"
   :description "Middleware for ring that helps with HTTP params"
-  :repl-options {:init-ns rs-data.repl})
+  :license "Eclipse Public License v 1.0")
